23cbadd9c944d3cb5cbbc6b74421164faae538b3
Soran Ghadri <kpg.kurd@gmail.com>
2020-11-29 05:04:41 +0330

    Organized emoji sets and added necessary comments

--- static/emoji.py
+++ static/emoji.py
@@ -1,25 +1,7 @@
 import emoji
 
-_traffic_light = emoji.EMOJI_ALIAS_UNICODE[':traffic_light:']
-_checkmark_emoji = emoji.EMOJI_ALIAS_UNICODE[':white_check_mark:']
-_floppy_emoji = emoji.EMOJI_ALIAS_UNICODE[':floppy_disk:']
-_clock_emoji = emoji.EMOJI_ALIAS_UNICODE[':hourglass_flowing_sand:']
-_lemon = emoji.EMOJI_ALIAS_UNICODE[':lemon:']
-_cherries = emoji.EMOJI_ALIAS_UNICODE[':cherries:']
-_apple = emoji.EMOJI_ALIAS_UNICODE[':apple:']
-_candy = emoji.EMOJI_ALIAS_UNICODE[':candy:']
-_watermelon = emoji.EMOJI_ALIAS_UNICODE[':watermelon:']
-_peach = emoji.EMOJI_ALIAS_UNICODE[':peach:']
-_strawberry = emoji.EMOJI_ALIAS_UNICODE[':strawberry:']
-_tea = emoji.EMOJI_ALIAS_UNICODE[':tea:']
-_cherry_blossom = emoji.EMOJI_ALIAS_UNICODE[':cherry_blossom:']
-_maple_leaf = emoji.EMOJI_ALIAS_UNICODE[':maple_leaf:']
-_pineapple = emoji.EMOJI_ALIAS_UNICODE[':pineapple:']
-_tangerine = emoji.EMOJI_ALIAS_UNICODE[':tangerine:']
-_grapes = emoji.EMOJI_ALIAS_UNICODE[':grapes:']
-_carrot = emoji.EMOJI_ALIAS_UNICODE[':carrot:']
-_ear_of_corn = emoji.EMOJI_ALIAS_UNICODE[':ear_of_corn:']
-_mushroom = emoji.EMOJI_ALIAS_UNICODE[':mushroom:']
+
+# -------------Country Flags--------------------
 _en = emoji.EMOJI_ALIAS_UNICODE[':United_States:']
 _fa = emoji.EMOJI_ALIAS_UNICODE[':Iran:']
 _ar = emoji.EMOJI_ALIAS_UNICODE[':Saudi_Arabia:']
@@ -27,10 +9,16 @@ _hi = emoji.EMOJI_ALIAS_UNICODE[':India:']
 _ru = emoji.EMOJI_ALIAS_UNICODE[':Russia:']
 _zap = emoji.EMOJI_ALIAS_UNICODE[':zap:']
 _globe_showing_Americas = emoji.EMOJI_ALIAS_UNICODE[':globe_showing_Americas:']
+
+# -------------Surprize--------------------------
 _party_popper = emoji.EMOJI_ALIAS_UNICODE[':party_popper:']
 _confetti_ball = emoji.EMOJI_ALIAS_UNICODE[':confetti_ball:']
+
+# -------------music--------------------------
 _headphone = emoji.EMOJI_ALIAS_UNICODE[':headphone:']
 _studio_microphone = emoji.EMOJI_ALIAS_UNICODE[':studio_microphone:']
+
+# -------------Utility and place--------------------------
 _round_pushpin = emoji.EMOJI_ALIAS_UNICODE[':round_pushpin:']
 _pushpin = emoji.EMOJI_ALIAS_UNICODE[':pushpin:']
 _search_emoji = emoji.EMOJI_ALIAS_UNICODE[':mag_right:']
@@ -54,15 +42,24 @@ _thumbs_up = emoji.EMOJI_ALIAS_UNICODE[':thumbs_up:']
 _thumbs_down = emoji.EMOJI_ALIAS_UNICODE[':thumbs_down:']
 _plus = emoji.EMOJI_ALIAS_UNICODE[':heavy_plus_sign:']
 _fountain_pen = emoji.EMOJI_ALIAS_UNICODE[':fountain_pen:']
+_traffic_light = emoji.EMOJI_ALIAS_UNICODE[':traffic_light:']
+_checkmark_emoji = emoji.EMOJI_ALIAS_UNICODE[':white_check_mark:']
+_floppy_emoji = emoji.EMOJI_ALIAS_UNICODE[':floppy_disk:']
+_clock_emoji = emoji.EMOJI_ALIAS_UNICODE[':hourglass_flowing_sand:']
+
+# -------------Books--------------------------
 _books = emoji.EMOJI_ALIAS_UNICODE[':books:']
 _book_emoji = emoji.EMOJI_ALIAS_UNICODE[':green_book:']
+
+# -------------Hearts--------------------------
 _red_heart = emoji.EMOJI_ALIAS_UNICODE[':red_heart:']
 _orange_heart = emoji.EMOJI_ALIAS_UNICODE[':orange_heart:']
 _yellow_heart = emoji.EMOJI_ALIAS_UNICODE[':yellow_heart:']
 _green_heart = emoji.EMOJI_ALIAS_UNICODE[':green_heart:']
 _blue_heart = emoji.EMOJI_ALIAS_UNICODE[':blue_heart:']
+
+# -------------Plants--------------------------
 _purple_heart = emoji.EMOJI_ALIAS_UNICODE[':purple_heart:']
-heart_list = [_red_heart, _orange_heart, _yellow_heart, _green_heart, _blue_heart, _purple_heart]
 _seedling = emoji.EMOJI_ALIAS_UNICODE[':seedling:']
 _evergreen_tree = emoji.EMOJI_ALIAS_UNICODE[':evergreen_tree:']
 _deciduous_tree = emoji.EMOJI_ALIAS_UNICODE[':deciduous_tree:']
@@ -73,6 +70,26 @@ _shamrock = emoji.EMOJI_ALIAS_UNICODE[':shamrock:']
 _four_leaf_clover = emoji.EMOJI_ALIAS_UNICODE[':four_leaf_clover:']
 _fallen_leaf = emoji.EMOJI_ALIAS_UNICODE[':fallen_leaf:']
 _leaf_fluttering_in_wind = emoji.EMOJI_ALIAS_UNICODE[':leaf_fluttering_in_wind:']
+
+# -------------Fruits--------------------------
+_apple = emoji.EMOJI_ALIAS_UNICODE[':apple:']
+_candy = emoji.EMOJI_ALIAS_UNICODE[':candy:']
+_watermelon = emoji.EMOJI_ALIAS_UNICODE[':watermelon:']
+_peach = emoji.EMOJI_ALIAS_UNICODE[':peach:']
+_strawberry = emoji.EMOJI_ALIAS_UNICODE[':strawberry:']
+_tea = emoji.EMOJI_ALIAS_UNICODE[':tea:']
+_cherry_blossom = emoji.EMOJI_ALIAS_UNICODE[':cherry_blossom:']
+_maple_leaf = emoji.EMOJI_ALIAS_UNICODE[':maple_leaf:']
+_pineapple = emoji.EMOJI_ALIAS_UNICODE[':pineapple:']
+_tangerine = emoji.EMOJI_ALIAS_UNICODE[':tangerine:']
+_grapes = emoji.EMOJI_ALIAS_UNICODE[':grapes:']
+_carrot = emoji.EMOJI_ALIAS_UNICODE[':carrot:']
+_ear_of_corn = emoji.EMOJI_ALIAS_UNICODE[':ear_of_corn:']
+_mushroom = emoji.EMOJI_ALIAS_UNICODE[':mushroom:']
+_lemon = emoji.EMOJI_ALIAS_UNICODE[':lemon:']
+_cherries = emoji.EMOJI_ALIAS_UNICODE[':cherries:']
+
+# -------------Faces--------------------------
 _smiling_face_with_heart = emoji.EMOJI_ALIAS_UNICODE[':smiling_face_with_heart-eyes:']
 _face_blowing_a_kiss = emoji.EMOJI_ALIAS_UNICODE[':face_blowing_a_kiss:']
 _face_with_raised_eyebrow = emoji.EMOJI_ALIAS_UNICODE[':face_with_raised_eyebrow:']
@@ -83,3 +100,4 @@ _winking_face = emoji.EMOJI_ALIAS_UNICODE[':winking_face:']
 fruit_list = [_grapes, _tangerine, _pineapple, _lemon, _cherries, _apple, _candy, _watermelon,
               _peach, _strawberry, _tea, _cherry_blossom, _maple_leaf, _carrot, _ear_of_corn, _mushroom]
 
+heart_list = [_red_heart, _orange_heart, _yellow_heart, _green_heart, _blue_heart, _purple_heart]
