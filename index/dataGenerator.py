fd137d28924e1cc2b0ec40063b1a2818a3511671
Soran Ghadri <kpg.kurd@gmail.com>
2020-12-12 21:21:01 +0330

    Implemented the user_data_generator

--- index/dataGenerator.py
+++ index/dataGenerator.py
@@ -7,3 +7,28 @@ def user_data_generator(message):
     :param message: The message containing the user, chat, etc. data
     :return:
     """
+    try:
+        user = message.from_user
+        yield {
+            "_index": "user",
+            "_id": user.id,
+            "_source": {
+                "first_name": user.first_name,
+                "username": user.username,
+                "date_joined": int(time.time()),
+                "downloaded_audio_id_list": [],
+                "favorite_audio_id_list": [], # when added to this list it'll be deleted from downloaded list
+                "downloaded_audio_count": 0,
+                "lang_code": "en",
+                "limited": False,  # after 5 songs users should wait 1 min till receive the file
+                "role": "searcher",
+                "coins": 0,
+                "last_active_date": int(time.time()),
+                "is_admin": False,
+                "sex": "neutral",
+                "country": "-",
+                "other": [],
+            }
+        }
+    except Exception as e:
+        print(f"Exception from data generator {e}")
