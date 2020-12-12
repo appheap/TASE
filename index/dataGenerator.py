dede2ebc7d35d799aedfcbc2783f33ad2c4439e3
Soran Ghadri <kpg.kurd@gmail.com>
2020-12-12 21:20:09 +0330

    Added docstring to dataGenerator.py

--- index/dataGenerator.py
+++ index/dataGenerator.py
@@ -1,1 +1,9 @@
 import time
+
+
+def user_data_generator(message):
+    """
+    A generator for user-data based on the message
+    :param message: The message containing the user, chat, etc. data
+    :return:
+    """
