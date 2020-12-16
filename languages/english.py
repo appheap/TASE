9c51bc619f84da86ee587df1a15778fb7caec365
Soran Ghadri <kpg.kurd@gmail.com>
2020-12-16 23:58:08 +0330

    Implemented music_file_keyboard function

--- languages/english.py
+++ languages/english.py
@@ -11,11 +11,13 @@ from static.emoji import _traffic_light, _checkmark_emoji, _floppy_emoji, _clock
     _exclamation_question_mark, _mobile_phone_with_arrow, _chart_increasing, _bar_chart, _check_mark_button, _gear, \
     _wrench, _cross_mark, _musical_note, _thumbs_up, _thumbs_down, _plus, _fountain_pen, _books, _red_heart, \
     _green_heart, heart_list, _seedling, _evergreen_tree, _deciduous_tree, _palm_tree, _sheaf_of_rice, _herb, _shamrock, \
-    _four_leaf_clover, _maple_leaf, _fallen_leaf, _leaf_fluttering_in_wind, _smiling_face_with_sunglasses, _winking_face, \
-    _smiling_face_with_heart, _face_blowing_a_kiss,_face_with_raised_eyebrow
+    _four_leaf_clover, _maple_leaf, _fallen_leaf, _leaf_fluttering_in_wind, _smiling_face_with_sunglasses, \
+    _winking_face, \
+    _smiling_face_with_heart, _face_blowing_a_kiss, _face_with_raised_eyebrow
 
 plants_list = [_seedling, _evergreen_tree, _deciduous_tree, _palm_tree, _sheaf_of_rice, _herb, _shamrock,
                _four_leaf_clover, _maple_leaf, _fallen_leaf, _leaf_fluttering_in_wind]
+
 
 def music_file_keyboard(*args, **kwargs):
     """
@@ -24,3 +26,11 @@ def music_file_keyboard(*args, **kwargs):
     :param kwargs:
     :return: Generated keyboard
     """
+
+    query = args[0]
+    keyboard = [
+        [InlineKeyboardButton(text=f"Add to playlist | {_plus}", switch_inline_query_current_chat=f"#addtopl: {query}"),
+         InlineKeyboardButton(text=f"Home | {_house}", callback_data="home")]
+
+    ]
+    return keyboard
