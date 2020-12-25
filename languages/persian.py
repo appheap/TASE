3d1cc018fa0f0a78084ae01cb89e45b4e0555755
Soran Ghadri <soran.gdr.cs@gmail.com>
2020-12-25 18:21:04 +0330

    Imported emoji codes from emoji.py

--- languages/persian.py
+++ languages/persian.py
@@ -5,3 +5,12 @@ from datetime import timedelta, datetime
 
 import emoji
 from pyrogram import InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
+
+from static.emoji import _traffic_light, _checkmark_emoji, _floppy_emoji, _clock_emoji, fruit_list, _en, _fa, _ar, _hi, \
+    _ru, _zap, _globe_showing_Americas, _party_popper, _confetti_ball, _headphone, _studio_microphone, _round_pushpin, \
+    _pushpin, _search_emoji, _house, _BACK_arrow, _star_struck, _green_circle, _backhand_index_pointing_right, \
+    _exclamation_question_mark, _mobile_phone_with_arrow, _chart_increasing, _bar_chart, _check_mark_button, _gear, \
+    _wrench, _cross_mark, _musical_note, _thumbs_up, _thumbs_down, _plus, _fountain_pen, _books, _red_heart, \
+    _green_heart, heart_list, _seedling, _evergreen_tree, _deciduous_tree, _palm_tree, _sheaf_of_rice, _herb, _shamrock, \
+    _four_leaf_clover, _maple_leaf, _fallen_leaf, _leaf_fluttering_in_wind, _smiling_face_with_heart, \
+    _face_blowing_a_kiss, _face_with_raised_eyebrow, _smiling_face_with_sunglasses, _winking_face, _artist_palette
