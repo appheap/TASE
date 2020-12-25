b2921bc5034bceb97eef58372648b1b616e11f40
Soran Ghadri <soran.gdr.cs@gmail.com>
2020-12-25 18:19:58 +0330

    Imported necessary packages2

--- languages/persian.py
+++ languages/persian.py
@@ -2,5 +2,6 @@ import random
 import textwrap
 import unicodedata as UD
 from datetime import timedelta, datetime
+
 import emoji
 from pyrogram import InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
