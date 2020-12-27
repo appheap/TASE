865eb81c21a0cae1a4d59b1b38fe119451c021a6
Soran Ghadri <soran.gdr.cs@gmail.com>
2020-12-27 21:54:21 +0330

    Returned the welcome results

--- languages/english.py
+++ languages/english.py
@@ -432,3 +432,4 @@ def welcome(*args, **kwargs):
            f"get your searching up to <b>#speed</b>.{_zap}\n" \
            f"\n\n{_studio_microphone} <b>#Find</b> your audio (music, podcast, etc.) in <b>#milliseconds</b> {_smiling_face_with_sunglasses}" \
            f"\n\n{_green_circle} Any question? then press /help {_winking_face}"
+    return text
