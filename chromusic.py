360bf7512b780cd6d8588ed0eb882cb21a0a65a2
Soran Ghadri <soran.gdr.cs@gmail.com>
2020-12-31 20:25:25 +0330

    Added copyright to chromusic.py module

---
+++ chromusic.py
@@ -0,0 +1,7 @@
+"""####################################################################
+# Copyright (C)                                                       #
+# 2020 Soran Ghadri(soran.gdr.cs@gmail.com)                           #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+####################################################################"""
+
