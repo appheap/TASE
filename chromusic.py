af36009662d828c7995eabe8234cadc7ed97e0d0
Soran Ghadri <soran.gdr.cs@gmail.com>
2020-12-31 20:26:18 +0330

    chromusic.py module:
    1. Imported necessary libraries
    
    chromusic.py module:
    
    1. Installed schedule library
    2. Installed elasticsearch library
    
    chromusic.py module:
    
    1. Added emojis
    
    chromusic.py module:
    
    1. Imported local emoji from static directory

--- chromusic.py
+++ chromusic.py
@@ -5,3 +5,25 @@
 # declaration at the top                                              #
 ####################################################################"""
 
+import concurrent.futures
+import random
+import secrets
+import textwrap
+from datetime import timedelta, datetime
+from uuid import uuid4
+
+# import emoji
+import schedule
+from elasticsearch import Elasticsearch, helpers
+from pyrogram import Client, Filters, InlineKeyboardMarkup, \
+    InputTextMessageContent, InlineQueryResultArticle
+# from search_module.search_handling import file_retrieve_handler
+from pyrogram.api import functions, types
+from pyrogram.errors import FloodWait, SlowmodeWait
+
+from static.emoji import _floppy_emoji, _clock_emoji
+from index.analyzer import channel_analyzer # Change this
+from index.dataGenerator import *
+# from languages.persian import result_list_handler
+from languages import english, persian
+from mapping import *
