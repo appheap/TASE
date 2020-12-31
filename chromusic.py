6f18bffd1e1878ca80a566ebfaf683b3abb210f0
Soran Ghadri <soran.gdr.cs@gmail.com>
2020-12-31 21:42:10 +0330

    telegramAPI_connect function: 1. Initiated the function 2. Added docstring
    telegramAPI_connect function:
    1. Modified api ids and hashes
    
    telegramAPI_connect function:
    1. Added details to the docstring of the function

--- chromusic.py
+++ chromusic.py
@@ -27,3 +27,38 @@ from index.dataGenerator import *
 # from languages.persian import result_list_handler
 from languages import english, persian
 from mapping import *
+
+def telegramAPI_connect():
+    """
+    This function makes multiple telegram app clients and their respective global variables to make them accessible
+    through the entire module.
+    You can customise this function and make app clients as much as you want.
+    :return:
+    """
+    global executor, app, app2, bot, adbot, indexer_list
+    indexer_list = []
+
+    chromusic_indexer_7693_api_id = "api_id_1"
+    chromusic_indexer_7693_api_hash = "api_hash_1"
+
+    shelbycobra2016_api_id = "api_id_2"
+    shelbycobra2016_api_hash = "api_hash_2"
+
+    api_id_0762 = "api_id_3"
+    api_hash_0762 = "api_hash_3"
+
+    api_id_0765 = "api_id_4"
+    api_hash_0765 = "api_hash_4"
+
+    BOT_TOKEN = "Your bot toke here"  # chromusic_bot bot token
+
+    # app_me = client_connect("shelbycobra2016", shelbycobra2016_api_id, shelbycobra2016_api_hash)
+    app = client_connect("Chromusic_1", api_id_0762, api_hash_0762)
+    app2 = client_connect("Chromusic_2", api_id_0765, api_hash_0765)
+    chromusic_indexer_7693 = client_connect("Chromusic_indexer_7693", chromusic_indexer_7693_api_id,
+                                            chromusic_indexer_7693_api_hash)
+
+    # adbot = adbot_connect(BOT_TOKEN, api_hash, api_id)
+    # bot = bot_connect("soranpythonbot", BOT_TOKEN, shelbycobra2016_api_hash, shelbycobra2016_api_id)
+    bot = bot_connect("chromusic_bot", shelbycobra2016_api_id, shelbycobra2016_api_hash, BOT_TOKEN)
+    indexer_list.append(chromusic_indexer_7693)
