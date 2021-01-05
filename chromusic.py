b4a6011ef0bef3802c2a3ece8b0db844f068a944
Soran <soran.gdr.cs@gmail.com>
2021-01-05 21:53:59 +0330

    file_retrieve_handler function: 1. Generate caption 2. Generate a probability for sending collaboration-request message

--- chromusic.py
+++ chromusic.py
@@ -533,5 +533,19 @@ def file_retrieve_handler(message):
         except Exception as e:
             audio_track = bot.get_messages(chat_id, message_id)  # message_id)
 
+        # _caption = str(english.file_caption(audio_track, message_id))
+        # print("audio track:")
+        user_data = es.get("user", id=user.id)["_source"]
+        lang_code = user_data["lang_code"]
+
+        collaboration_request_message = language_handler("collaboration_request", lang_code)
+        probability = 0
+        if random.random() > 0.8:
+            probability = 1
+
+        # print("audio track:", lang_code)
+        # _caption = caption_handler(audio_track, message_id, lang_code)
+        _caption = language_handler("file_caption", lang_code, audio_track, audio_track.message_id)
+        music_file_keyboard = language_handler("music_file_keyboard", lang_code, query)
     except:
         ""
