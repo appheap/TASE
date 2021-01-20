8f2d8c19d47f99ccff4b5aab96bf6d2941630d36
Soran <soran.gdr.cs@gmail.com>
2021-01-20 19:24:07 +0330

    daily_gathered_channels_controller function: 1. Implement main body

--- chromusic.py
+++ chromusic.py
@@ -940,3 +940,15 @@ def daily_gathered_channels_controller(client):
     :param client:
     :return:
     """
+    while 1:
+        _channels_list_username = []
+        print("existing channels now running  ...")
+        es.indices.refresh("future_channel")
+        res = helpers.scan(es,
+                           query={"query": {"match_all": {}}},
+                           index="future_channel")
+
+        for future_channel_instance in res:
+            _channels_list_username.append(future_channel_instance["_id"])
+        # print("new indexing channels started ... !")
+        new_channel_indexer(client, _channels_list_username, "future_channel")
