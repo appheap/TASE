73211822606a5778880ac170f19206de358442ac
Soran <soran.gdr.cs@gmail.com>
2021-01-25 18:29:59 +0330

    existing_channels_handler_by_importance function: 1. Add more details to the docstring

--- chromusic.py
+++ chromusic.py
@@ -972,9 +972,9 @@ def daily_gathered_channels_controller(client: object) -> bool:
 def existing_channels_handler_by_importance(client, importance):
     """
     This function retrieves channels from DB by importance and updates their indexing status
-    :param client:
-    :param importance:
-    :return:
+    :param client: Telegram client
+    :param importance: the target importance of the channels to be retrieved from DB
+    :return: -
     """
     try:
         while 1:
