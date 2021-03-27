2ab0e9ae3d8e056e4c675ba8797af3d16214eefc
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-03-27 19:42:39 +0430

    inline_res function: 1. Answer the inline search query

--- chromusic.py
+++ chromusic.py
@@ -2602,3 +2602,15 @@ def inine_res(bot: object, query: object) -> object:
                                    f"{hidden_character}{_floppy_emoji} | {round(int(hit['_source']['file_size']) / 1_048_576, 1)} MB  " \
                                    f"{_clock_emoji} | {str(d.hour) + ':' if d.hour > 0 else ''}{d.minute}:{d.second}"  # 1000_000 MB
                 item_title = hidden_character + str(index + 1) + '. ' + _title
+
+                results.append(InlineQueryResultArticle(
+                    title=item_title,
+                    # description=res["_source"]["performer"],
+                    description=item_describtion,
+                    thumb_url="https://telegra.ph/file/cd08f00005cb527e6bcdb.jpg",
+                    # "https://www.howtogeek.com/wp-content/uploads/2017/09/img_59b89568ec308.jpg",
+                    input_message_content=InputTextMessageContent(_caption_content, parse_mode="HTML")))
+
+        exception_handler(
+            bot.answer_inline_query(query.id, results=results,
+                                    cache_time=10, switch_pm_text=back_text, switch_pm_parameter="back_to_the_bot"))
