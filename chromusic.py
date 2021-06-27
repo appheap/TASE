a45730378995920e43dc96bb924dec1da0a43eaf
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-06-27 14:56:08 +0430

    callback_query_handler function: 1. Add home option

--- chromusic.py
+++ chromusic.py
@@ -2976,6 +2976,17 @@ def callback_query_handler(bot, query):
                                                   reply_markup=InlineKeyboardMarkup(markup_list),
                                                   parse_mode='HTML'))
 
+    elif str(query.data) == "home":
+        home_markup_keyboard = language_handler("home_markup_keyboard", user_data["lang_code"])
+        home_keyboard_text = language_handler("home_keyboard_text", user_data["lang_code"])
+        # exception_handler(bot.send_message(chat_id=user.id,
+        #                                     text=mylists_menu_text,
+        #                                    reply_markup=InlineKeyboardMarkup(markup_list),
+        #                                    parse_mode='HTML'))
+        exception_handler(query.edit_message_text(text=home_keyboard_text,
+                                                  reply_markup=InlineKeyboardMarkup(home_markup_keyboard),
+                                                  parse_mode='HTML'))
+
 def show_playlist(query, user_data):
     """
     Generates a keyboard for each playlist; buttons:
