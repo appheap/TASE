c11f75c7df6d541fb3141cecf49d262bf18df325
sigma1326 <taleb.zarhesh@gmail.com>
2022-04-17 23:00:39 +0430

    + Moved `telegram_client` module from `tase/models/` package to `tase/telegram/` package

---
+++ tase/telegram/__init__.py
@@ -0,0 +1,15 @@
+from .telegram_client import TelegramClient
+from .telegram_client import ClientTypes
+from .telegram_client import UserTelegramClient
+from .telegram_client import BotTelegramClient
+from .telegram_client import BotClientRoles
+from .telegram_client import UserClientRoles
+
+__all__ = [
+    "TelegramClient",
+    "ClientTypes",
+    "UserTelegramClient",
+    "BotTelegramClient",
+    "BotClientRoles",
+    "UserClientRoles",
+]
