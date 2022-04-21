09bf75fdcfc895510d7cb07cf826d59463f3eed0
sigma1326 <taleb.zarhesh@gmail.com>
2022-04-22 00:32:18 +0430

    + Updated `base_task` module in `tase/telegram/tasks/` package

--- tase/telegram/tasks/base_task.py
+++ tase/telegram/tasks/base_task.py
@@ -1,8 +1,19 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Callable
 
 from tase.db.database_client import DatabaseClient
+from tase.my_logger import logger
 from tase.telegram import TelegramClient
+
+
+def exception_handler(func: 'Callable'):
+    def wrap(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as e:
+            logger.exception(e)
+
+    return wrap
 
 
 @dataclass
@@ -11,5 +22,6 @@ class BaseTask:
     args: List[object] = field(default_factory=list)
     kwargs: dict = field(default_factory=dict)
 
+    @exception_handler
     def run_task(self, telegram_client: 'TelegramClient', db: 'DatabaseClient'):
         pass
