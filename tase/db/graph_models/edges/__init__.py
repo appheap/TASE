8e5e438251477302f813aa4ff678970a14267886
sigma1326 <taleb.zarhesh@gmail.com>
2022-04-21 15:08:26 +0430

    + Added `sender_chat` module to `tase/db/graph_models/edges/` package

--- tase/db/graph_models/edges/__init__.py
+++ tase/db/graph_models/edges/__init__.py
@@ -1,7 +1,9 @@
 from .base_edge import BaseEdge
 from .file_ref import FileRef
+from .sender_chat import SenderChat
 
 __all__=[
     'BaseEdge',
     'FileRef',
+    'SenderChat',
 ]
---
+++ tase/db/graph_models/edges/sender_chat.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import arrow
+
+from .base_edge import BaseEdge
+from ..vertices import Audio, Chat
+
+
+@dataclass
+class SenderChat(BaseEdge):
+    """Connection from `Audio` to `Chat`"""
+
+    @staticmethod
+    def parse_from_audio_and_chat(audio: 'Audio', chat: 'Chat') -> Optional['SenderChat']:
+        if audio is None and chat is None:
+            return None
+
+        ts = int(arrow.utcnow().timestamp())
+        return SenderChat(
+            id=None,
+            key=None,
+            from_node=audio,
+            to_node=chat,
+            created_at=ts,
+            modified_at=ts,
+        )
