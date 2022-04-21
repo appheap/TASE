c748684f0e6d9707c209ceb03481ee942624f0dd
sigma1326 <taleb.zarhesh@gmail.com>
2022-04-21 15:06:14 +0430

    + Added `base_edge` module to `tase/db/graph_models/edges/` package

---
+++ tase/db/graph_models/edges/__init__.py
@@ -0,0 +1,5 @@
+from .base_edge import BaseEdge
+
+__all__=[
+    'BaseEdge',
+]
---
+++ tase/db/graph_models/edges/base_edge.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from ..vertices import BaseVertex
+
+
+@dataclass
+class BaseEdge:
+    id: Optional[str]
+    key: Optional[str]
+    from_node: 'BaseVertex'
+    to_node: 'BaseVertex'
+    created_at: int
+    modified_at: int
+
+    def parse_for_graph(self) -> dict:
+        return {
+            '_id': self.id,
+            '_key': self.key,
+            '_from': self.from_node.id,
+            '_to': self.to_node.id,
+            'created_at': self.created_at,
+            'modified_at': self.modified_at,
+        }
