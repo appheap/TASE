a2e88a32db0c0c1536f831959f032a568bc73851
sigma1326 <taleb.zarhesh@gmail.com>
2022-04-21 14:46:57 +0430

    + Added `file` module to `tase/db/graph_models/vertices/` package

--- tase/db/graph_models/vertices/__init__.py
+++ tase/db/graph_models/vertices/__init__.py
@@ -1,9 +1,11 @@
 from .base_vertex import BaseVertex
+from .file import File
 from .restriction import Restriction
 from .user import User
 
 __all__ = [
     'BaseVertex',
+    'File',
     'Restriction',
     'User',
 ]
---
+++ tase/db/graph_models/vertices/file.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+import arrow
+import pyrogram
+
+from .base_vertex import BaseVertex
+
+
+@dataclass
+class File(BaseVertex):
+    _vertex_name = 'files'
+
+    file_unique_id: str
+
+    def parse_for_graph(self) -> dict:
+        super_dict = super(File, self).parse_for_graph()
+        super_dict.update(
+            {
+                'file_unique_id': self.file_unique_id,
+            }
+        )
+
+        return super_dict
+
+    @staticmethod
+    def parse_from_audio(audio: 'pyrogram.types.Audio'):
+        if audio is None:
+            return None
+
+        ts = int(arrow.utcnow().timestamp())
+        return File(
+            id=None,
+            key=audio.file_unique_id,
+            rev=None,
+            created_at=ts,
+            modified_at=ts,
+            file_unique_id=audio.file_unique_id,
+        )
