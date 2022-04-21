a352925b75970f47cdd327a901ce3628a3aeaa02
sigma1326 <taleb.zarhesh@gmail.com>
2022-04-22 00:04:22 +0430

    + Updated `base_vertex` module in `tase/db/graph_models/vertices/` package

--- tase/db/graph_models/vertices/base_vertex.py
+++ tase/db/graph_models/vertices/base_vertex.py
@@ -20,3 +20,21 @@ class BaseVertex:
             'created_at': self.created_at,
             'modified_at': self.modified_at,
         }
+
+    @staticmethod
+    def parse_from_graph(vertex: dict) -> Optional['dict']:
+        if not len(vertex):
+            return None
+
+        return {
+            'id': vertex.get('_id', None),
+            'key': vertex.get('_key', None),
+            'rev': vertex.get('_rev', None),
+            'created_at': vertex.get('created_at', None),
+            'modified_at': vertex.get('modified_at', None),
+        }
+
+    def update_from_metadata(self, metadata: dict):
+        self.id = metadata.get('_id', None)
+        self.key = metadata.get('_key', None)
+        self.rev = metadata.get('_rev', None)
