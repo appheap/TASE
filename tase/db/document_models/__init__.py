e9a692683794a2995e93febc685ffec5d976eb29
sigma1326 <taleb.zarhesh@gmail.com>
2022-05-01 00:09:54 +0430

    + Added `audio` module to `tase/db/document_models/` package

--- tase/db/document_models/__init__.py
+++ tase/db/document_models/__init__.py
@@ -1,10 +1,12 @@
+from .audio import Audio
 from .base_document import BaseDocument
 
 __all__ = [
     'BaseDocument',
+    'Audio',
 
 ]
 
 docs = [
-
+    Audio,
 ]
---
+++ tase/db/document_models/audio.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+import pyrogram
+
+from .base_document import BaseDocument
+from .. import elasticsearch_models
+
+
+class Audio(BaseDocument):
+    _doc_collection_name = 'doc_audios'
+
+    chat_id: int
+    message_id: int
+
+    file_id: str
+    file_unique_id: str
+
+    @staticmethod
+    def get_key(message: 'pyrogram.types.Message', telegram_client_id: int):
+        return f'{str(telegram_client_id)}:{message.audio.file_unique_id}:{message.chat.id}:{message.id}'
+
+    @staticmethod
+    def get_key_from_audio(audio: 'elasticsearch_models.Audio', telegram_client_id: int):
+        return f'{str(telegram_client_id)}:{audio.file_unique_id}:{audio.chat_id}:{audio.message_id}'
+
+    @staticmethod
+    def parse_from_message(message: 'pyrogram.types.Message', telegram_client_id: int) -> Optional['Audio']:
+        if not message or not message.audio or telegram_client_id is None:
+            return None
+
+        key = Audio.get_key(message, telegram_client_id)
+        return Audio(
+            key=key,
+            chat_id=message.chat.id,
+            message_id=message.id,
+            file_id=message.audio.file_id,
+            file_unique_id=message.audio.file_unique_id,
+        )
