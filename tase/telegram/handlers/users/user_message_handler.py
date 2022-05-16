4d263d6fca70891fcfa1774f4f18276bbc93f8fa
sigma1326 <taleb.zarhesh@gmail.com>
2022-05-16 17:09:46 +0430

    + Bugfix

--- tase/telegram/handlers/users/user_message_handler.py
+++ tase/telegram/handlers/users/user_message_handler.py
@@ -21,15 +21,7 @@ class UserMessageHandler(BaseHandler):
     def user_message_handler(self, client: 'pyrogram.Client', message: 'pyrogram.types.Message'):
         direction = '=>' if message.outgoing else '<='
         logger.debug(f"user_message_handler: {direction} {message.chat.title or message.chat.first_name}")
-        if message.chat.id == 61709467:
-            if not message.media:
-                try:
-                    logger.info(self.task_queues)
-                    q = self.task_queues[self.telegram_client.name]
-                    chat = client.get_chat(chat_id=message.text)
-                    client_task(
-                        IndexAudiosTask(kwargs={'chat_id': chat.id}),
-                        target_queue=q
-                    )
-                except Exception as e:
-                    logger.exception(e)
+
+        # todo: temp solution
+        from .temp import func
+        func(client, self.telegram_client, message, logger, self.task_queues)
