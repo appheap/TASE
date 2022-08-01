42859946cd668df3c8915c7c9dc55ba087ebcc6a
sigma1326 <taleb.zarhesh@gmail.com>
2022-08-01 20:20:03 +0430

    + Fix bug

--- tase/static/emoji.py
+++ tase/static/emoji.py
@@ -18,127 +18,111 @@ from pydantic import BaseModel
 
 class Emoji(BaseModel):
     # -------------Country Flags--------------------
-    _usa_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":United_States:"]
-    _tajikistan_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Tajikistan:"]
-    _lithuania_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Lithuania:"]
-    _italy_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Italy:"]
-    _spain_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Spain:"]
-    _iran_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Iran:"]
-    _saudi_arabia_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Saudi_Arabia:"]
-    _india_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":India:"]
-    _russia_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Russia:"]
-    _germany_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Germany:"]
-    _netherlands_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Netherlands:"]
-    _portugal_flag = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":Portugal:"]
-    _zap = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":zap:"]
-    _globe_showing_Americas = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[
-        ":globe_showing_Americas:"
-    ]
+    _usa_flag = emoji.emojize(":United_States:")
+    _tajikistan_flag = emoji.emojize(":Tajikistan:")
+    _lithuania_flag = emoji.emojize(":Lithuania:")
+    _italy_flag = emoji.emojize(":Italy:")
+    _spain_flag = emoji.emojize(":Spain:")
+    _iran_flag = emoji.emojize(":Iran:")
+    _saudi_arabia_flag = emoji.emojize(":Saudi_Arabia:")
+    _india_flag = emoji.emojize(":India:")
+    _russia_flag = emoji.emojize(":Russia:")
+    _germany_flag = emoji.emojize(":Germany:")
+    _netherlands_flag = emoji.emojize(":Netherlands:")
+    _portugal_flag = emoji.emojize(":Portugal:")
+    _zap = emoji.emojize(":zap:")
+    _globe_showing_Americas = emoji.emojize(":globe_showing_Americas:")
 
-    # -------------Surprize--------------------------
-    _party_popper = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":party_popper:"]
-    _confetti_ball = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":confetti_ball:"]
+    # -------------Surprise--------------------------
+    _party_popper = emoji.emojize(":party_popper:")
+    _confetti_ball = emoji.emojize(":confetti_ball:")
 
     # -------------music--------------------------
-    _headphone = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":headphone:"]
+    _headphone = emoji.emojize(":headphone:")
 
-    _studio_microphone = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":studio_microphone:"]
+    _studio_microphone = emoji.emojize(":studio_microphone:")
 
     # -------------Utility and place--------------------------
-    _round_pushpin = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":round_pushpin:"]
-    _pushpin = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":pushpin:"]
-    _search_emoji = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":mag_right:"]
-    _house_with_garden = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":house_with_garden:"]
-    _house = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":house:"]
-    _BACK_arrow = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":BACK_arrow:"]
-    _star_struck = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":star-struck:"]
-    _artist_palette = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":artist_palette:"]
-    _green_circle = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":green_circle:"]
-    _backhand_index_pointing_right = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[
-        ":left_arrow_curving_right:"
-    ]
-    _exclamation_question_mark = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[
-        ":exclamation_question_mark:"
-    ]
-    _mobile_phone_with_arrow = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[
-        ":mobile_phone_with_arrow:"
-    ]
-    _chart_increasing = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":chart_increasing:"]
-    _bar_chart = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":bar_chart:"]
-    _check_mark_button = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":white_check_mark:"]
-    _gear = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":gear:"]
-    _wrench = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":wrench:"]
-    _cross_mark = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":cross_mark:"]
-    _musical_note = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":musical_note:"]
-    _thumbs_up = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":thumbs_up:"]
-    _thumbs_down = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":thumbs_down:"]
-    _plus = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":heavy_plus_sign:"]
-    _minus = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":heavy_minus_sign:"]
-    _fountain_pen = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":fountain_pen:"]
-    _traffic_light = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":traffic_light:"]
-    _checkmark_emoji = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":white_check_mark:"]
-    _floppy_emoji = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":floppy_disk:"]
-    _clock_emoji = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":hourglass_flowing_sand:"]
+    _round_pushpin = emoji.emojize(":round_pushpin:")
+    _pushpin = emoji.emojize(":pushpin:")
+    _search_emoji = emoji.emojize(":mag_right:")
+    _house_with_garden = emoji.emojize(":house_with_garden:")
+    _house = emoji.emojize(":house:")
+    _BACK_arrow = emoji.emojize(":BACK_arrow:")
+    _star_struck = emoji.emojize(":star-struck:")
+    _artist_palette = emoji.emojize(":artist_palette:")
+    _green_circle = emoji.emojize(":green_circle:")
+    _backhand_index_pointing_right = emoji.emojize(":left_arrow_curving_right:")
+    _exclamation_question_mark = emoji.emojize(":exclamation_question_mark:")
+    _mobile_phone_with_arrow = emoji.emojize(":mobile_phone_with_arrow:")
+    _chart_increasing = emoji.emojize(":chart_increasing:")
+    _bar_chart = emoji.emojize(":bar_chart:")
+    _check_mark_button = emoji.emojize(":white_check_mark:")
+    _gear = emoji.emojize(":gear:")
+    _wrench = emoji.emojize(":wrench:")
+    _cross_mark = emoji.emojize(":cross_mark:")
+    _musical_note = emoji.emojize(":musical_note:")
+    _thumbs_up = emoji.emojize(":thumbs_up:")
+    _thumbs_down = emoji.emojize(":thumbs_down:")
+    _plus = emoji.emojize(":heavy_plus_sign:")
+    _minus = emoji.emojize(":heavy_minus_sign:")
+    _fountain_pen = emoji.emojize(":fountain_pen:")
+    _traffic_light = emoji.emojize(":traffic_light:")
+    _checkmark_emoji = emoji.emojize(":white_check_mark:")
+    _floppy_emoji = emoji.emojize(":floppy_disk:")
+    _clock_emoji = emoji.emojize(":hourglass_flowing_sand:")
 
     # -------------Books--------------------------
-    _books = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":books:"]
-    _book_emoji = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":green_book:"]
+    _books = emoji.emojize(":books:")
+    _book_emoji = emoji.emojize(":green_book:")
 
     # -------------Hearts--------------------------
-    _red_heart = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":red_heart:"]
-    _orange_heart = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":orange_heart:"]
-    _yellow_heart = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":yellow_heart:"]
-    _green_heart = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":green_heart:"]
-    _blue_heart = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":blue_heart:"]
+    _red_heart = emoji.emojize(":red_heart:")
+    _orange_heart = emoji.emojize(":orange_heart:")
+    _yellow_heart = emoji.emojize(":yellow_heart:")
+    _green_heart = emoji.emojize(":green_heart:")
+    _blue_heart = emoji.emojize(":blue_heart:")
 
     # -------------Plants--------------------------
-    _purple_heart = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":purple_heart:"]
-    _seedling = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":seedling:"]
-    _evergreen_tree = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":evergreen_tree:"]
-    _deciduous_tree = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":deciduous_tree:"]
-    _palm_tree = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":palm_tree:"]
-    _sheaf_of_rice = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":sheaf_of_rice:"]
-    _herb = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":herb:"]
-    _shamrock = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":shamrock:"]
-    _four_leaf_clover = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":four_leaf_clover:"]
-    _fallen_leaf = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":fallen_leaf:"]
-    _leaf_fluttering_in_wind = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[
-        ":leaf_fluttering_in_wind:"
-    ]
+    _purple_heart = emoji.emojize(":purple_heart:")
+    _seedling = emoji.emojize(":seedling:")
+    _evergreen_tree = emoji.emojize(":evergreen_tree:")
+    _deciduous_tree = emoji.emojize(":deciduous_tree:")
+    _palm_tree = emoji.emojize(":palm_tree:")
+    _sheaf_of_rice = emoji.emojize(":sheaf_of_rice:")
+    _herb = emoji.emojize(":herb:")
+    _shamrock = emoji.emojize(":shamrock:")
+    _four_leaf_clover = emoji.emojize(":four_leaf_clover:")
+    _fallen_leaf = emoji.emojize(":fallen_leaf:")
+    _leaf_fluttering_in_wind = emoji.emojize(":leaf_fluttering_in_wind:")
 
     # -------------Fruits--------------------------
-    _apple = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":apple:"]
-    _candy = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":candy:"]
-    _watermelon = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":watermelon:"]
-    _peach = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":peach:"]
-    _strawberry = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":strawberry:"]
-    _tea = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":tea:"]
-    _cherry_blossom = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":cherry_blossom:"]
-    _maple_leaf = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":maple_leaf:"]
-    _pineapple = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":pineapple:"]
-    _tangerine = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":tangerine:"]
-    _grapes = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":grapes:"]
-    _carrot = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":carrot:"]
-    _ear_of_corn = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":ear_of_corn:"]
-    _mushroom = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":mushroom:"]
-    _lemon = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":lemon:"]
-    _cherries = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":cherries:"]
+    _apple = emoji.emojize(":apple:")
+    _candy = emoji.emojize(":candy:")
+    _watermelon = emoji.emojize(":watermelon:")
+    _peach = emoji.emojize(":peach:")
+    _strawberry = emoji.emojize(":strawberry:")
+    _tea = emoji.emojize(":tea:")
+    _cherry_blossom = emoji.emojize(":cherry_blossom:")
+    _maple_leaf = emoji.emojize(":maple_leaf:")
+    _pineapple = emoji.emojize(":pineapple:")
+    _tangerine = emoji.emojize(":tangerine:")
+    _grapes = emoji.emojize(":grapes:")
+    _carrot = emoji.emojize(":carrot:")
+    _ear_of_corn = emoji.emojize(":ear_of_corn:")
+    _mushroom = emoji.emojize(":mushroom:")
+    _lemon = emoji.emojize(":lemon:")
+    _cherries = emoji.emojize(":cherries:")
 
     # -------------Faces--------------------------
-    _smiling_face_with_heart = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[
-        ":smiling_face_with_heart-eyes:"
-    ]
-    _face_blowing_a_kiss = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":face_blowing_a_kiss:"]
-    _face_with_raised_eyebrow = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[
-        ":face_with_raised_eyebrow:"
-    ]
-    _smiling_face_with_sunglasses = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[
-        ":smiling_face_with_sunglasses:"
-    ]
-    _winking_face = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":winking_face:"]
+    _smiling_face_with_heart = emoji.emojize(":smiling_face_with_heart-eyes:")
+    _face_blowing_a_kiss = emoji.emojize(":face_blowing_a_kiss:")
+    _face_with_raised_eyebrow = emoji.emojize(":face_with_raised_eyebrow:")
+    _smiling_face_with_sunglasses = emoji.emojize(":smiling_face_with_sunglasses:")
+    _winking_face = emoji.emojize(":winking_face:")
 
     # ------------Others-----------------------------------
-    high_voltage = emoji.EMOJI_ALIAS_UNICODE_ENGLISH[":high_voltage:"]
+    high_voltage = emoji.emojize(":high_voltage:")
 
     # a list to randomly choose from
     fruit_list = [
