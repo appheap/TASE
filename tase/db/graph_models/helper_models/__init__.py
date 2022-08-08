875bad8f51cfebb1b37ee973a6edf42713d66601
sigma1326 <taleb.zarhesh@gmail.com>
2022-08-08 14:14:37 +0430

    + Update docs

--- tase/db/graph_models/helper_models/__init__.py
+++ tase/db/graph_models/helper_models/__init__.py
@@ -1,3 +1,8 @@
+"""
+This package is used to include models that are neither `vertex` nor `edge`, but they are being used in either a
+`vertex` or an `edge`.
+"""
+
 from .indexer_metadata import IndexerMetadata
 from .restriction import Restriction
 
