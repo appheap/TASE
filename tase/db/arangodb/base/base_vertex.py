e76fef799cc252d2f8c1fb895e13b01e73a62ea1
sigma1326 <taleb.zarhesh@gmail.com>
2022-08-15 16:01:16 +0430

    + Update codes

--- tase/db/arangodb/base/base_vertex.py
+++ tase/db/arangodb/base/base_vertex.py
@@ -1,5 +1,9 @@
+from arango.collection import VertexCollection
+from pydantic.typing import Optional
+
 from .base_collection_document import BaseCollectionDocument
 
 
 class BaseVertex(BaseCollectionDocument):
     _collection_name = "base_vertices"
+    _collection: Optional[VertexCollection]
