21212423f8fbca7be51d2a31d563256806ecfbe3
sigma1326 <taleb.zarhesh@gmail.com>
2022-08-18 19:31:49 +0430

    + Update codes

--- tase/db/arangodb/graph/edges/has.py
+++ tase/db/arangodb/graph/edges/has.py
@@ -1,4 +1,4 @@
-from pydantic.typing import Optional, Union
+from pydantic.typing import Optional, Union, Tuple
 
 from .base_edge import BaseEdge
 from ..vertices import User, Playlist, Query, InlineQuery, Hit, Download, Audio, Keyword
@@ -56,4 +56,45 @@ class Has(BaseEdge):
 
 
 class HasMethods:
-    pass
+    def create_has_edge(
+        self,
+        from_vertex: Union[User, Playlist, Query, InlineQuery, Hit, Download],
+        to_vertex: Union[Playlist, Audio, Hit, Keyword],
+    ) -> Tuple[Optional[Has], bool]:
+        if from_vertex is None or to_vertex is None:
+            return None, False
+
+        return Has.insert(Has.parse(from_vertex, to_vertex))
+
+    def get_or_create_has_edge(
+        self,
+        from_vertex: Union[User, Playlist, Query, InlineQuery, Hit, Download],
+        to_vertex: Union[Playlist, Audio, Hit, Keyword],
+    ) -> Optional[Has]:
+        if from_vertex is None or to_vertex is None:
+            return None
+
+        has_edge = Has.get(Has.parse_key(from_vertex, to_vertex))
+        if not has_edge:
+            # has_edge does not exist in the database, create it
+            has_edge, successful = self.create_has_edge(from_vertex, to_vertex)
+
+        return has_edge
+
+    def update_or_create_has_edge(
+        self,
+        from_vertex: Union[User, Playlist, Query, InlineQuery, Hit, Download],
+        to_vertex: Union[Playlist, Audio, Hit, Keyword],
+    ) -> Optional[Has]:
+        if from_vertex is None or to_vertex is None:
+            return None
+
+        has_edge = Has.get(Has.parse_key(from_vertex, to_vertex))
+        if has_edge is not None:
+            # has_edge exists in the database, update it
+            has_edge, successful = has_edge.update(Has.parse(from_vertex, to_vertex))
+        else:
+            # has_edge does not exist in the database, create it
+            has_edge, successful = self.create_has_edge(from_vertex, to_vertex)
+
+        return has_edge
