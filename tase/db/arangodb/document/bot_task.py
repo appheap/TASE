a5e44696b5540246cf7b02c3feb9ff60b4d8a4b4
sigma1326 <taleb.zarhesh@gmail.com>
2022-09-13 22:27:50 +0430

    + Update codes
    Added methods to the `BotTask` module document in `tase/db/arangodb/document` package
    
    Resolves #12 and #13

--- tase/db/arangodb/document/bot_task.py
+++ tase/db/arangodb/document/bot_task.py
@@ -1,5 +1,12 @@
+from __future__ import annotations
+
+import uuid
+from typing import Optional
+
+from arango import CursorEmptyError
 from pydantic import Field
 
+from tase.my_logger import logger
 from .base_document import BaseDocument
 from ..enums import BotTaskType, BotTaskStatus
 
@@ -69,6 +76,192 @@ class BotTask(BaseDocument):
         else:
             raise Exception("Could not update retry count field")
 
+    @classmethod
+    def parse(
+        cls,
+        user_id: int,
+        bot_id: int,
+        task_type: BotTaskType,
+        state_dict: dict = None,
+    ) -> Optional[BotTask]:
+        if user_id is None or bot_id is None or task_type is None:
+            return None
+
+        bot_task = BotTask(
+            key=str(uuid.uuid4()),
+            user_id=user_id,
+            bot_id=bot_id,
+            type=task_type,
+        )
+        if state_dict is not None and len(state_dict):
+            bot_task.state_dict = state_dict
+
+        return bot_task
+
+    def update_task_state_dict(
+        self,
+        new_task_state: dict,
+    ) -> bool:
+        """
+        Update the task extra parameters dictionary.
+
+        Parameters
+        ----------
+        new_task_state : dict
+            Dictionary of the new extra parameters
+
+        Returns
+        -------
+        bool
+            Whether the update was successful or not
+        """
+        if new_task_state is None:
+            return False
+
+        self_copy = self.copy(deep=True)
+        self_copy.state_dict = new_task_state
+        return self.update(self_copy, reserve_non_updatable_fields=True)
+
 
 class BotTaskMethods:
-    pass
+    _cancel_recent_bot_tasks_query = (
+        "for doc_task in @doc_bot_tasks"
+        "   sort doc_task.modified_at desc"
+        "   filter doc_task.type == @type and doc_task.status == @status and doc_task.user_id == @user_id and doc_task.bot_id == @bot_id"
+        "   update doc_task with {"
+        "       status: @new_status"
+        "   } in @doc_bot_tasks options {mergeObjects: true}"
+        "   return NEW"
+    )
+
+    _get_latest_bot_task_query = (
+        "for doc_task in @doc_bot_tasks"
+        "   filter doc_task.status == @status and doc_task.user_id == @user_id and doc_task.bot_id == @bot_id"
+        "   sort doc_task.modified_at desc"
+        "   limit 1"
+        "   return doc_task"
+    )
+
+    def create_bot_task(
+        self,
+        user_id: int,
+        bot_id: int,
+        task_type: BotTaskType,
+        state_dict: dict = None,
+    ) -> Optional[BotTask]:
+        """
+        Create a `BotTask` document in the ArangoDB.
+
+        Parameters
+        ----------
+        user_id : int
+            ID of the Telegram User
+        bot_id : int
+            ID of the BOT this task belongs to
+        task_type : BotTaskType
+            Type of the task
+        state_dict : dict
+            Extra data passed to the BotTask
+
+        Returns
+        -------
+        BotTask, optional
+            BotTask document if the creation was successful, otherwise, return None
+        """
+        task, successful = BotTask.insert(
+            BotTask.parse(
+                user_id,
+                bot_id,
+                task_type,
+                state_dict,
+            )
+        )
+        if task and successful:
+            return task
+
+        return None
+
+    def cancel_recent_bot_task(
+        self,
+        user_id: int,
+        bot_id: int,
+        task_type: BotTaskType,
+    ) -> bool:
+        """
+        Cancel the most recent bot task for the specified user.
+
+        Parameters
+        ----------
+        user_id : int
+            ID of the Telegram User to cancel the task for
+        bot_id : int
+            ID of the Telegram Bot which the task made for
+        task_type : BotTaskType
+            Type of the BotTask to cancel
+
+        Returns
+        -------
+        bool
+            Whether the cancellation was successful or not.
+        """
+        if user_id is None or bot_id is None or task_type is None:
+            return False
+
+        cursor = BotTask.execute_query(
+            self._cancel_recent_bot_tasks_query,
+            bind_vars={
+                "doc_bot_tasks": BotTask._collection_name,
+                "type": task_type.value,
+                "user_id": user_id,
+                "bot_id": bot_id,
+                "status": BotTaskStatus.CREATED.value,
+                "new_status": BotTaskStatus.CANCELED.value,
+            },
+        )
+        if cursor is not None:
+            return True
+        else:
+            return False
+
+    def get_latest_bot_task(
+        self,
+        user_id: int,
+        bot_id: int,
+    ) -> Optional[BotTask]:
+        """
+        Get the latest created bot task for the specified user id.
+
+        Parameters
+        ----------
+        user_id : int
+            ID of the Telegram User that this task belongs to
+        bot_id : int
+            ID of the Telegram Bot that the task made for
+
+        Returns
+        -------
+        BotTask, optional
+            Latest BotTask for the given user ID if it exists, otherwise, return None
+        """
+        if user_id is None or bot_id is None:
+            return
+
+        cursor = BotTask.execute_query(
+            self._get_latest_bot_task_query,
+            bind_vars={
+                "doc_bot_tasks": BotTask._collection_name,
+                "user_id": user_id,
+                "bot_id": bot_id,
+                "status": BotTaskStatus.CREATED.value,
+            },
+        )
+        if cursor is not None and len(cursor):
+            try:
+                bot_task = BotTask.from_collection(cursor.pop())
+                return bot_task
+            except CursorEmptyError:
+                pass
+            except Exception as e:
+                logger.exception(e)
+
+        return None
