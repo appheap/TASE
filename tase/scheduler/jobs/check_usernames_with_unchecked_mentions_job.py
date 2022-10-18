5b27548abdedaabc14399e7c83b7c39f422e38b3
sigma1326 <taleb.zarhesh@gmail.com>
2022-10-18 13:47:46 +0330

    + Add a job to check for usernames with unchecked mentions at regular intervals

---
+++ tase/scheduler/jobs/check_usernames_with_unchecked_mentions_job.py
@@ -0,0 +1,44 @@
+import time
+
+import arrow
+from apscheduler.triggers.interval import IntervalTrigger
+from kombu.mixins import ConsumerProducerMixin
+
+import tase
+from .base_job import BaseJob
+from ...db.arangodb.enums import RabbitMQTaskType
+from ...telegram.client import TelegramClient
+from ...telegram.tasks import CheckUsernameTask
+
+
+class CheckUsernamesWithUncheckedMentionsJob(BaseJob):
+    type = RabbitMQTaskType.CHECK_USERNAMES_WITH_UNCHECKED_MENTIONS_JOB
+
+    trigger = IntervalTrigger(
+        hours=1,
+        start_date=arrow.now().shift(seconds=+20).datetime,
+    )
+
+    def run(
+        self,
+        consumer: ConsumerProducerMixin,
+        db: tase.db.DatabaseClient,
+        telegram_client: TelegramClient = None,
+    ) -> None:
+        self.task_in_worker(db)
+        usernames = db.graph.get_checked_usernames_with_unchecked_mentions()
+
+        for idx, username in enumerate(usernames):
+            # todo: blocking or non-blocking? which one is better suited for this case?
+
+            if idx > 0 and idx % 10 == 0:
+                # fixme: sleep to avoid publishing many tasks while the others haven't been processed yet
+                time.sleep(10 * 15)
+
+            CheckUsernameTask(
+                kwargs={
+                    "username_key": username.key,
+                }
+            ).publish(db)
+
+        self.task_done(db)
