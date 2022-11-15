74a038c05469a3359c703aade64caa2e0b1744d1
sigma1326 <taleb.zarhesh@gmail.com>
2022-11-15 16:08:01 +0330

    + Add more classes

--- aioarango/errors/server/batch_errors/__init__.py
+++ aioarango/errors/server/batch_errors/__init__.py
@@ -4,10 +4,8 @@ Batch Execution Exceptions
 
 from .batch_execute_error import BatchExecuteError
 from .batch_job_result_error import BatchJobResultError
-from .batch_state_error import BatchStateError
 
 __all__ = [
     "BatchExecuteError",
     "BatchJobResultError",
-    "BatchStateError",
 ]
