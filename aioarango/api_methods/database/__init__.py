93f57ae4aef676a1897d4af82709ac9f95eadfb2
sigma1326 <taleb.zarhesh@gmail.com>
2022-11-18 01:02:12 +0330

    + Add `list_accessible_databases` module to `aioarango/api_methods/database` package

--- aioarango/api_methods/database/__init__.py
+++ aioarango/api_methods/database/__init__.py
@@ -1,9 +1,11 @@
 from .create_database import CreateDatabase
+from .list_accessible_databases import ListAccessibleDatabases
 from .list_of_databases import ListOfDatabases
 
 
 class DatabaseMethods(
     CreateDatabase,
+    ListAccessibleDatabases,
     ListOfDatabases,
 ):
     pass
