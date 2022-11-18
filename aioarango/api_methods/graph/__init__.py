9e4f1c39939cf18a207d1340bc25d6ed7afca0c8
sigma1326 <taleb.zarhesh@gmail.com>
2022-11-18 22:29:41 +0330

    + Add `get_graph` module to `aioarango/api_methods/graph` package

--- aioarango/api_methods/graph/__init__.py
+++ aioarango/api_methods/graph/__init__.py
@@ -1,9 +1,11 @@
 from .create_graph import CreateGraph
+from .get_graph import GetGraph
 from .list_all_graphs import ListAllGraphs
 
 
 class GraphMethods(
     ListAllGraphs,
+    GetGraph,
     CreateGraph,
 ):
     pass
---
+++ aioarango/api_methods/graph/get_graph.py
@@ -0,0 +1,60 @@
+from aioarango.api import Endpoint
+from aioarango.enums import MethodType
+from aioarango.errors import ArangoServerError, ErrorType
+from aioarango.models import Graph, Request, Response
+from aioarango.typings import Result
+
+
+class GetGraph:
+    error_codes = (ErrorType.GRAPH_NOT_FOUND,)
+    status_codes = (
+        200,
+        # Returns the graph if it could be found.
+        404,  # 1924
+        # Returned if no graph with this name could be found.
+    )
+
+    async def get_graph(
+        self: Endpoint,
+        name: str,
+    ) -> Result[Graph]:
+        """
+        Get a graph from the ArangoDB.
+
+        Parameters
+        ----------
+        name : str
+            Name of the graph.
+
+        Returns
+        -------
+        Result
+            Graph object if the operation was successful.
+
+        Raises
+        ------
+        ValueError
+            If the input graph name is invalid.
+        aioarango.errors.ArangoServerError
+            If retrieval fails.
+
+        """
+        if name is None or not len(name):
+            raise ValueError("`name` has invalid value!")
+
+        request = Request(
+            method_type=MethodType.GET,
+            endpoint=f"/_api/gharial/{name}",
+        )
+
+        def response_handler(response: Response) -> Graph:
+            if not response.is_success:
+                raise ArangoServerError(response, request)
+
+            # status_code 200
+            if "graph" not in response.body:
+                raise ValueError("`name` has invalid value!")
+
+            return Graph.parse_graph_dict(response.body["graph"])
+
+        return await self.execute(request, response_handler)
