013f66000ce1fa7b7c5b3c62d09d02ab0d3b4134
sigma1326 <taleb.zarhesh@gmail.com>
2022-11-19 01:05:03 +0330

    + Add `list_edge_definitions` module to `aioarango/api_methods/graph` package

--- aioarango/api_methods/graph/__init__.py
+++ aioarango/api_methods/graph/__init__.py
@@ -2,12 +2,14 @@ from .create_graph import CreateGraph
 from .drop_graph import DropGraph
 from .get_graph import GetGraph
 from .list_all_graphs import ListAllGraphs
+from .list_edge_definitions import ListEdgeDefinitions
 
 
 class GraphMethods(
-    ListAllGraphs,
+    CreateGraph,
     DropGraph,
     GetGraph,
-    CreateGraph,
+    ListAllGraphs,
+    ListEdgeDefinitions,
 ):
     pass
---
+++ aioarango/api_methods/graph/list_edge_definitions.py
@@ -0,0 +1,64 @@
+from typing import List, Dict
+
+from aioarango.api import Endpoint
+from aioarango.enums import MethodType
+from aioarango.errors import ArangoServerError, ErrorType
+from aioarango.models import Request, Response, EdgeDefinition
+from aioarango.typings import Result
+
+
+class ListEdgeDefinitions:
+    error_codes = (ErrorType.GRAPH_NOT_FOUND,)
+    status_codes = (
+        200,
+        # Is returned if the edge definitions could be listed.
+        404,  # 1924
+        # Returned if no graph with this name could be found.
+    )
+
+    async def list_edge_definitions(
+        self: Endpoint,
+        name: str,
+    ) -> Result[List[EdgeDefinition]]:
+        """
+        List all edge collections within this graph.
+
+
+        Parameters
+        ----------
+        name : str
+            Name of the graph.
+
+        Returns
+        -------
+        Result
+            Edge definitions of the graph.
+
+        Raises
+        ------
+        ValueError
+            If graph name is invalid.
+        aioarango.errors.ArangoServerError
+            If retrieval fails.
+
+        """
+        if name is None or not len(name):
+            raise ValueError("`name` has invalid value!")
+
+        request = Request(
+            method_type=MethodType.GET,
+            endpoint=f"/_api/gharial/{name}",
+        )
+
+        def response_handler(response: Response) -> List[EdgeDefinition]:
+            if not response.is_success:
+                raise ArangoServerError(response, request)
+
+            # status_code 200
+            if "graph" not in response.body:
+                raise ValueError("`name` has invalid value!")
+
+            body: Dict = response.body["graph"]
+            return [EdgeDefinition.parse_edge_definition_dict(edge_definition) for edge_definition in body["edgeDefinitions"]]
+
+        return await self.execute(request, response_handler)
