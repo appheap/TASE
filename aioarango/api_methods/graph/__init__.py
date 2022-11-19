32682919c6796b93b4c4e3c4ab2c7cfc232b0681
sigma1326 <taleb.zarhesh@gmail.com>
2022-11-19 17:28:11 +0330

    + Add `create_edge` module to `aioarango/api_methods/graph` package + Add `create_vertex` module to `aioarango/api_methods/graph` package

--- aioarango/api_methods/graph/__init__.py
+++ aioarango/api_methods/graph/__init__.py
@@ -1,6 +1,8 @@
 from .add_edge_definition import AddEdgeDefinition
 from .add_vertex_collection import AddVertexCollection
+from .create_edge import CreateEdge
 from .create_graph import CreateGraph
+from .create_vertex import CreateVertex
 from .drop_graph import DropGraph
 from .get_graph import GetGraph
 from .list_all_graphs import ListAllGraphs
@@ -13,7 +15,9 @@ from .remove_vertex_collection import RemoveVertexCollection
 class GraphMethods(
     AddEdgeDefinition,
     AddVertexCollection,
+    CreateEdge,
     CreateGraph,
+    CreateVertex,
     DropGraph,
     GetGraph,
     ListAllGraphs,
---
+++ aioarango/api_methods/graph/create_vertex.py
@@ -0,0 +1,107 @@
+from typing import Optional
+
+from aioarango.api import Endpoint
+from aioarango.enums import MethodType
+from aioarango.errors import ArangoServerError, ErrorType
+from aioarango.models import Request, Response
+from aioarango.typings import Json, Result, Params
+from aioarango.utils.document_utils import ensure_key_from_id
+from aioarango.utils.graph_utils import format_vertex
+
+
+class CreateVertex:
+    error_types = (
+        ErrorType.ARANGO_DOCUMENT_KEY_BAD,
+        ErrorType.GRAPH_NOT_FOUND,
+        ErrorType.GRAPH_REFERENCED_VERTEX_COLLECTION_NOT_USED,
+    )
+    status_codes = (
+        201,
+        # Returned if the vertex could be added and `waitForSync` is true.
+        202,
+        # Returned if the request was successful but `waitForSync` is false.
+        400,  # 1221
+        403,
+        # Returned if your user has insufficient rights.
+        # In order to insert vertices into the graph you at least need to have the following privileges:
+        #   1. `Read Only `access on the Database.
+        #   2. `Write` access on the given collection.
+        404,  # 1924, 1947
+        # Returned if no graph with this name could be found.
+        # Or if a graph is found but this collection is not part of the graph
+    )
+
+    async def create_vertex(
+        self: Endpoint,
+        graph_name: str,
+        vertex_collection_name: str,
+        id_prefix: str,
+        vertex: Json,
+        return_new: Optional[bool] = False,
+        wait_for_sync: Optional[bool] = None,
+    ) -> Result:
+        """
+        Add a vertex to the given collection.
+
+        Parameters
+        ----------
+        graph_name : str
+            Name of the graph.
+        vertex_collection_name : str
+            Name of the vertex collection to add the vertex to.
+        id_prefix : str
+            ID prefix for this vertex.
+        vertex : Json
+            New vertex document to insert. If it has "_key" or "_id"
+            field, its value is used as key of the new vertex (otherwise it is
+            auto-generated). Any "_rev" field is ignored.
+        return_new : bool, default : False
+            Include body of the new document in the returned metadata.
+        wait_for_sync : bool, optional
+            Block until operation is synchronized to disk.
+
+        Returns
+        -------
+        Result
+            Document metadata (e.g. document key, revision).
+
+        Raises
+        ------
+        ValueError
+            If graph name or vertex collection name is invalid.
+        aioarango.errors.DocumentParseError
+            If collection name is invalid or the body is `None`.
+        aioarango.errors.ArangoServerError
+            If operation fails.
+        """
+        if graph_name is None or not len(graph_name):
+            raise ValueError(f"`graph_name` has invalid value: `{graph_name}`")
+
+        if vertex_collection_name is None or not len(vertex_collection_name):
+            raise ValueError(f"`vertex_collection_name` has invalid value: `{vertex_collection_name}`")
+
+        vertex = ensure_key_from_id(vertex, id_prefix)
+
+        params: Params = {}
+        if wait_for_sync is not None:
+            params["waitForSync"] = wait_for_sync
+
+        if return_new is not None:
+            params["returnNew"] = return_new
+
+        request = Request(
+            method_type=MethodType.POST,
+            endpoint=f"/_api/gharial/{graph_name}/vertex/{vertex_collection_name}",
+            data=vertex,
+            params=params if len(params) else None,
+            write=vertex_collection_name,
+        )
+
+        def response_handler(response: Response):
+            if not response.is_success:
+                raise ArangoServerError(response, request)
+
+            # status_code 201, 202
+            return format_vertex(response.body)
+
+        return await self.execute(request, response_handler)
