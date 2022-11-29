498bb937c9e4b1b59dad22a8f6fe66fe80076a66
sigma1326 <taleb.zarhesh@gmail.com>
2022-11-29 15:09:27 +0330

    + Add `explain_aql_query` module to `aioarango/api_methods/aql` package + Update codes

--- aioarango/api_methods/aql/__init__.py
+++ aioarango/api_methods/aql/__init__.py
@@ -1,4 +1,5 @@
 from .clear_slow_aql_queries import ClearSlowAQLQueries
+from .explain_aql_query import ExplainAQLQuery
 from .get_running_aql_queries import GetRunningAQLQueries
 from .get_slow_aql_queries import GetSlowAQLQueries
 from .kill_running_aql_query import KillRunningAQLQuery
@@ -8,6 +9,7 @@ from ..cursors import CreateCursor  # fixme: this class belongs to `cursor` api 
 
 class AQLMethods(
     ClearSlowAQLQueries,
+    ExplainAQLQuery,
     GetRunningAQLQueries,
     GetSlowAQLQueries,
     KillRunningAQLQuery,
